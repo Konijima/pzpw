47f1b576edfd8999e8e60db0f340f5bf41ee2550
Konijima <mathieugermain1991@gmail.com>
2022-08-22 14:13:48 -0400

    Add requirePZPWProject and  help command

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -4,7 +4,7 @@ import { existsSync } from 'fs';
 import { homedir } from 'os';
 import { join } from 'path';
 import { Settings } from './settings.js';
-import { getPZPWConfig } from './utils.js';
+import { getCommandHelp, getHelp, getIntro, getPZPWConfig } from './utils.js';
 
 export class Cli {
     
@@ -17,13 +17,21 @@ export class Cli {
     }
 
     /**
-     * Start the compiler process
+     * Start the cli process
      */
      public async run() {
         this.settings = await Settings.Load();
         this.pzpwConfig = await getPZPWConfig().catch(() => {});
 
         await this.exec();
+    }
+
+    /**
+     * Verify that the process is running inside a PZPW project.
+     */
+    private requirePZPWProject() {
+        if (!this.pzpwConfig)
+            throw chalk.red('This command must be executed from the root of your PZPW project.');
     }
 
     /**
@@ -45,7 +53,20 @@ export class Cli {
     private async exec() {
         let command = this.getCommand();
 
-        
+        const shortIntro = (!command.name || command.name === 'help');
+        await getIntro().then(text => console.log(chalk.greenBright((!shortIntro) ? text.split('\n').slice(0, 4).join('\n') : text)));
+
+        // Debug Flag
+        if (this.args.debug) {
+            console.log(chalk.magenta("Command:"), command);
+            console.log(chalk.magenta("Settings:"), this.settings.settings, '\n');
+        }
+
+        if (command.name === "help" && command.params.length > 0)
+            await getCommandHelp(command.params[0] as string, true).then(text => console.log(chalk.grey(text)))
+                .catch(_ => console.log(chalk.grey(`Command "${command.params[0] as string}" not found!`)));
+
+        else await getHelp().then(text => console.log(chalk.grey(text)));
     }
 
 }
